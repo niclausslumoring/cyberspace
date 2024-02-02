758b205adf903dae908e545ab1b5bdde191ba8b6
Niclauss Lumoring <103998426+niclausslumoring@users.noreply.github.com>
2024-02-02 11:59:37 +0700

    slider

--- js/partner-slider.js
+++ js/partner-slider.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let currentIndex = 0;
 
   function showImage(index) {
+    var active = [];
     const images = slider.children;
     const screenWidth = window.innerWidth;
 
@@ -16,8 +17,48 @@ document.addEventListener("DOMContentLoaded", function () {
       const adjustedIndex = (index + i) % images.length;
       if (i >= 0 && i < numVisibleImages) {
         images[adjustedIndex].classList.remove("hidden");
+        active.push(adjustedIndex);
       } else {
         images[adjustedIndex].classList.add("hidden");
+      }
+    }
+    // console.log(active);
+    if (JSON.stringify(active) === JSON.stringify([4, 5, 0])) {
+      // console.log("TET");
+      slider.innerHTML = `
+      <img src="temp-asset/partnership/sevilla.png" alt="" class="image__slider" />
+      <img src="temp-asset/partnership/btb.png" alt="" class="image__slider" />
+      <img src="temp-asset/partnership/uph.png" alt="" class="image__slider" />
+      <img src="temp-asset/partnership/hope.png" alt="" class="image__slider hidden" />
+      <img src="temp-asset/partnership/gatau.png" alt="" class="image__slider hidden" />
+      <img src="temp-asset/partnership/bbs.png" alt="" class="image__slider hidden" />            
+      `;
+    } else if (JSON.stringify(active) === JSON.stringify([5, 0, 1])) {
+      // console.log("TET");
+      slider.innerHTML = `
+      <img src="temp-asset/partnership/btb.png" alt="" class="image__slider" />
+      <img src="temp-asset/partnership/uph.png" alt="" class="image__slider" />
+      <img src="temp-asset/partnership/hope.png" alt="" class="image__slider" />
+      <img src="temp-asset/partnership/gatau.png" alt="" class="image__slider hidden" />
+      <img src="temp-asset/partnership/bbs.png" alt="" class="image__slider hidden" />
+      <img src="temp-asset/partnership/sevilla.png" alt="" class="image__slider hidden" />
+      `;
+    } else {
+      slider.innerHTML = `
+      <img src="temp-asset/partnership/uph.png" alt="" class="image__slider" />
+      <img src="temp-asset/partnership/hope.png" alt="" class="image__slider" />
+      <img src="temp-asset/partnership/gatau.png" alt="" class="image__slider" />
+      <img src="temp-asset/partnership/bbs.png" alt="" class="image__slider" />
+      <img src="temp-asset/partnership/sevilla.png" alt="" class="image__slider" />
+      <img src="temp-asset/partnership/btb.png" alt="" class="image__slider" />
+      `;
+      for (let i = 0; i < images.length; i++) {
+        const adjustedIndex = (index + i) % images.length;
+        if (i >= 0 && i < numVisibleImages) {
+          images[adjustedIndex].classList.remove("hidden");
+        } else {
+          images[adjustedIndex].classList.add("hidden");
+        }
       }
     }
   }
@@ -26,7 +67,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const screenWidth = window.innerWidth; // Tambahkan ini
     const numVisibleImages = screenWidth >= 640 ? 3 : 1;
     currentIndex = (currentIndex - 1 + slider.children.length) % slider.children.length;
-    showImage(currentIndex - numVisibleImages + 1);
+    showImage(currentIndex);
+    console.log(currentIndex);
   }
 
   function nextImage() {
@@ -34,6 +76,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const numVisibleImages = screenWidth >= 640 ? 3 : 1;
     currentIndex = (currentIndex + 1) % slider.children.length;
     showImage(currentIndex);
+    console.log(currentIndex);
   }
 
   prevBtn.addEventListener("click", prevImage);
